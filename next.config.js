6be4a847fa0ad08282fa9c5a20677ddfb089598b
MrjazTm <63402636+mrjaztm@users.noreply.github.com>
2025-02-21 17:52:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
+
 import frontmatter from 'gray-matter'
 import { languageKeys } from '#src/languages/lib/languages.js'
 import { ROOT } from '#src/frame/lib/constants.js'
